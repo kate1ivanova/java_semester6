ec25953b418d472caa0eec1ea40d7e91749a0410
kate <Ekaterina.Ivanoova@yandex.ru>
2025-04-07 17:42:52 +0300

    beginning code without timing

--- java3/src/main/java/org/example/Main.java
+++ java3/src/main/java/org/example/Main.java
@@ -1,17 +1,50 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    private static final int OPERATION_COUNT = 10000;
+
+    public static void main(String[] args) {
+        int smallSize = 1000;
+        int mediumSize = 10000;
+        int largeSize = 100000;
+
+        System.out.println("Демонстрация операций с ArrayList и LinkedList");
+        System.out.println("Количество операций: " + OPERATION_COUNT);
+        System.out.println("-----------------------------------------------");
+
+        demonstrateOperations(smallSize, "Small (" + smallSize + " elements)");
+        demonstrateOperations(mediumSize, "Medium (" + mediumSize + " elements)");
+        demonstrateOperations(largeSize, "Large (" + largeSize + " elements)");
+    }
+
+    private static void demonstrateOperations(int initialSize, String testName) {
+        System.out.println("\n" + testName);
+        System.out.println("-----------------------------------------------------");
+        System.out.printf("%-15s | %-15s\n", "Operation", "List Type");
+        System.out.println("-----------------------------------------------------");
+
+        demonstrateAdd(initialSize);
+        demonstrateGet(initialSize);
+        demonstrateRemove(initialSize);
+    }
+
+    private static void demonstrateAdd(int initialSize) {
+        System.out.printf("%-15s | %-15s\n", "add", "ArrayList");
+        System.out.printf("%-15s | %-15s\n", "add", "LinkedList");
+    }
+
+    private static void demonstrateGet(int initialSize) {
+        System.out.printf("%-15s | %-15s\n", "get", "ArrayList");
+        System.out.printf("%-15s | %-15s\n", "get", "LinkedList");
+    }
+
+    private static void demonstrateRemove(int initialSize) {
+        System.out.printf("%-15s | %-15s\n", "remove", "ArrayList");
+        System.out.printf("%-15s | %-15s\n", "remove", "LinkedList");
     }
 }
