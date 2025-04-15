7645e5631e13076a838e73ba3f0c47e8a8dd6a7b
kate <Ekaterina.Ivanoova@yandex.ru>
2025-04-15 19:04:11 +0300

    adding class Main

--- java4/src/main/java/org/example/Main.java
+++ java4/src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
 package org.example;
 
-public class Main {
 
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        CSVFileReader csvReader = new CSVFileReader();
+        try {
+
+            List<Person> people = csvReader.readPeopleFromCSV("foreign_names.csv", ';');
+            // Выводим результат
+            for (Person person : people) {
+                System.out.println(person);
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
